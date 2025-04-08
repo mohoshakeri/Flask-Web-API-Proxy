d66c94b6fd8665ccc7496b240ac94053a9e796e4
Hossein Shakeri <hossein82shakeri@gmail.com>
2025-04-08 13:27:46 +0330

    INIT

---
+++ main.py
@@ -0,0 +1,65 @@
+import requests
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+
+@app.route(
+    "/",
+    defaults={"path": ""},
+    methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+)
+@app.route("/path:path", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"])
+def proxy(path):
+    target_base = request.args.get("targetProxyServer")
+    if not target_base:
+        return "targetProxyServer not founded", 400
+
+    # Clean Path
+    if path:
+        target_url = target_base.rstrip("/") + "/" + path
+    else:
+        target_url = target_base
+
+    # GET Params
+    params = dict(request.args)
+    params.pop("targetProxyServer", None)
+
+    # Headers
+    headers = {}
+    for key, value in request.headers:
+        if key.lower() == "host":
+            continue
+        headers[key] = value
+
+    # Body
+    data = request.get_data()
+
+    try:
+        # Request
+        resp = requests.request(
+            method=request.method,
+            url=target_url,
+            params=params,
+            headers=headers,
+            data=data,
+            cookies=request.cookies,
+            allow_redirects=False,
+        )
+    except requests.exceptions.RequestException as e:
+        return f"Target Server Error -> {str(e)}", 502
+
+    # Response
+    response = Response(resp.content, status=resp.status_code)
+
+    # Response Headers
+    excluded_headers = ["content-encoding", "transfer-encoding", "connection"]
+    for name, value in resp.headers.items():
+        if name.lower() not in excluded_headers:
+            response.headers[name] = value
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
